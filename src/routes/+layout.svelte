60f77081b1cdf93708e83b920200df7c301e2df3
JackBell-p <87609650+JackBell-p@users.noreply.github.com>
2026-02-27 21:58:05 +0800

    update the view of the program

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  
+</script>
+
+<div class="container">
+  <header class="header">
+    <ul>
+      <li><a href="/">流量</a></li>
+      <li><a href="/log">日志</a></li>
+      <li><a href="/about">关于</a></li>
+      <li><a href="/donation">打赏</a></li>
+    </ul>
+  </header>
+
+  <main>
+    <slot />
+  </main>
+</div>
+
+<style>
+  @import "./+page.css";
+</style>
