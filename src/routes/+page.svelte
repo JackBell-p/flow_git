a75734b19a3ecaf76ec7ffe95024db0fc96fd69e
JackBell-p <3452896431@qq.com>
2026-03-03 23:23:31 +0800

    finish the interface of data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,50 @@
 <script lang="ts">
     import { onMount } from "svelte";
-    import { init } from "echarts";
+    import * as echarts from "echarts";
 
     let chart_div: HTMLDivElement;
 
     onMount(() => {
+        const chart = echarts.init(chart_div);
 
+        chart.setOption({
+            title: { text: "网络流量监控" },
+            tooltip: { trigger: "axis" },
+            legend: { data: ["上行流量", "下行流量"] },
+            xAxis: {
+                type: "category",
+                data: ["08:00:00", "08:05:00", "08:10:00", "08:15:00"]
+            },
+            yAxis: {
+                type: "value",
+                name: "B/s",
+                min: 0,
+                max: 18
+            },
+            series: [
+                {
+                    name: "上行流量",
+                    type: "line",
+                    data: [0, 2, 4, 0],
+                    color: "blue",
+                    smooth: true
+                },
+                {
+                    name: "下行流量",
+                    type: "line",
+                    data: [0, 5, 10, 0],
+                    color: "red",
+                    smooth: true
+                }
+            ]
+        });
+
+        window.addEventListener("resize", () => chart.resize());
     });
 </script>
 
-<div bind:this={chart_div}></div>
+<div bind:this={chart_div} class="chart"></div>
+
+<style>
+    @import "./+page.css";
+</style>
